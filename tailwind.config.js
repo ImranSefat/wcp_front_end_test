7d3f856f45b5cfdaa75d063e98a3afd1bcf86f6a
Imran Sefat <imransefat@gmail.com>
2022-08-18 23:12:49 +1000

    Transform on y axis added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+// Rotate Y utilities
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities({
+    '.rotate-y-20': {
+      transform: 'rotateY(20deg)',
+    },
+    '.rotate-y-40': {
+      transform: 'rotateY(40deg)',
+    },
+    '.rotate-y-60': {
+      transform: 'rotateY(60deg)',
+    },
+    '.rotate-y-180': {
+      transform: 'rotateY(180deg)',
+    },
+  })
+})
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,5 +26,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    rotateY
+  ],
 }
